b9d6ce9b322cda53e8b6ee73f96641a59a39b0ad
chenqiao <bigjoechan@163.com>
2021-12-04 17:22:21 +0800

    Create TraceSpan.java

---
+++ esTracingModule/TraceSpan.java
@@ -0,0 +1,84 @@
+package es;
+
+public class TraceSpan {
+
+    private String traceId;
+
+    private String spanId;
+
+    private String parentId;
+
+    private String start;
+
+    private String endTime;
+
+    private String serviceName;
+
+    private String from;
+
+    private String to;
+
+    public String getTraceId() {
+        return traceId;
+    }
+
+    public void setTraceId(String traceId) {
+        this.traceId = traceId;
+    }
+
+    public String getSpanId() {
+        return spanId;
+    }
+
+    public void setSpanId(String spanId) {
+        this.spanId = spanId;
+    }
+
+    public String getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(String parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+}
